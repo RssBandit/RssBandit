c2c783fef228e042bd1db2f39c1b386c06305912
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2007-10-25 01:20:27 +0000

    Raised version number to 1.5.0.18

--- source/RssBandit.AppServices/VersionInfo.cs
+++ source/RssBandit.AppServices/VersionInfo.cs
@@ -18,13 +18,13 @@ using System.Reflection;
 //      Revision (like SP's for a build)
 //
 // Please raise Build Number on each significant (e.g. interface changes) modification !!!
-[assembly: AssemblyVersion("1.5.0.10")]
+[assembly: AssemblyVersion("1.5.0.18")]
 
 // This attribute attaches version information to 
 // an assembly file to allow version verification by users.
 // Please raise Build Number on each modification !!!
-[assembly: AssemblyFileVersion("1.5.0.15")]
+[assembly: AssemblyFileVersion("1.5.0.18")]
 
 // This attribute attaches additional version information to 
 // an assembly for documentation purposes only.
-[assembly: AssemblyInformationalVersion("1.5.15")]
+[assembly: AssemblyInformationalVersion("1.5.18")]
