ad154ccf604e0c10f11cc37da3f3afbbbe024fb1
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2008-01-16 16:01:04 +0000

    Made first steps towards implementing Feed Sources.

---
+++ source/NewsComponents/Feed/BanditNewsHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsComponents.Feed {
+    /// <summary>
+    /// A NewsHandler that directly accesses RSS/Atom feeds via HTTP or HTTPS 
+    /// and newsgroups via NNTP. 
+    /// </summary>
+    class BanditNewsHandler : NewsHandler{
+    }
+}
