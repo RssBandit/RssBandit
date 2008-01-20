c3baa97a3c9cb0398f408e309d6224e5c1c2bae3
onovotny <onovotny@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-01-20 16:21:26 +0000

    Added WPF prototype

---
+++ trunk/source/Prototypes/RssBandit2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IEControl;
+
+namespace RssBandit2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            htmlControl.Navigate("http://www.cnn.com");
+        }
+
+
+
+    }
+}
