c722eca01229b55c9655fafe808542722a538394
onovotny <onovotny@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-01-20 18:10:40 +0000

    Factored WebBrowser out of main window

---
+++ trunk/source/Prototypes/RssBandit2/WebBrowser.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace RssBandit2
+{
+    /// <summary>
+    /// Interaction logic for WebBrowser.xaml
+    /// </summary>
+    [ContentProperty("Html")]
+    public partial class WebBrowser : UserControl
+    {
+        public WebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private static void SetControlHtml(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var wb = (WebBrowser)obj;
+
+            var str = (string)args.NewValue;
+
+            if (string.IsNullOrEmpty(str))
+                wb.htmlControl.Clear();
+            else
+            {
+                wb.htmlControl.Html = str;
+                wb.htmlControl.Navigate(null);
+            }
+        }
+
+
+        public string Html
+        {
+            get { return (string)GetValue(HtmlProperty); }
+            set { SetValue(HtmlProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Html.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HtmlProperty =
+            DependencyProperty.Register("Html", typeof(string), typeof(WebBrowser), new UIPropertyMetadata(null, SetControlHtml));
+
+
+    }
+}
