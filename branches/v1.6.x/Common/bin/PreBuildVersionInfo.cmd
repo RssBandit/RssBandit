a611e37831a6c2edf9ffc5f8f00c1a8011f2a99e
onovotny <onovotny@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-01-27 17:30:59 +0000

    Removed file/folder

--- branches/v1.6.x/Common/bin/PreBuildVersionInfo.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-rem --------------------------------------------------------------
-rem Call PrebuildVersionInfo.cmd 
-rem Call PrebuildVersionInfo.cmd with "<my_fileversion_path>"
-rem --------------------------------------------------------------
-cScript.exe //E:vbscript "D:\My Projects\DOT.NET\Sourceforge.RssBandit\CurrentWork\Source\Common\bin\GenerateVersionInfo.vbs" %1 %2 > prebuild.report.txt
-
