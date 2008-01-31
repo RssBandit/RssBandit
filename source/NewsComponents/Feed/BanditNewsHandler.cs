043c4cc0b75e419128d612eedb9a852090a61865
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2008-01-31 14:56:27 +0000

    Fixed issue where NewsHandler.Categories was not being populated on loading the feed list

--- source/NewsComponents/Feed/BanditNewsHandler.cs
+++ source/NewsComponents/Feed/BanditNewsHandler.cs
@@ -127,7 +127,7 @@ namespace NewsComponents.Feed
         /// Loads the RSS feedlist from the given URL and validates it against the schema. 
         /// </summary>
         /// <param name="xmlStream">The XML Stream of a feedlist to load</param>
-        /// <param name="veh">The event handler that should be invoked on the client if validation errors occur</param>
+        /// <param name="veh">The event handler that should be invoked on the client if validation errors occur. NOT USED.</param>
         /// <exception cref="XmlException">XmlException thrown if XML is not well-formed</exception>
         private void LoadFeedlist(Stream xmlStream, ValidationEventHandler veh)
         {
@@ -140,213 +140,202 @@ namespace NewsComponents.Feed
             XmlSerializer serializer = XmlHelper.SerializerCache.GetSerializer(typeof(feeds));
             feeds myFeeds = (feeds)serializer.Deserialize(reader);
             reader.Close();
+           
 
-
-            if (!validationErrorOccured)
+            //copy over category info if we are importing a new feed
+            if (myFeeds.categories != null)
             {
-                //copy feeds over if we are importing a new feed  
-
-                if (myFeeds.feed != null)
+                foreach (category cat in myFeeds.categories)
                 {
-                    foreach (NewsFeed f in myFeeds.feed)
+                    string cat_trimmed = cat.Value.Trim();
+                    if (!this.Categories.ContainsKey(cat_trimmed))
                     {
-                        if (FeedsTable.ContainsKey(f.link) == false)
-                        {
-                            bool isBadUri = false;
-                            try
-                            {
-                                Uri uri = new Uri(f.link);
-                                // CLR 2.0 Uri does not like "news:" scheme, so we 
-                                // switch it to "nntp:" (see http://msdn2.microsoft.com/en-us/library/system.uri.scheme.aspx)
-                                if (NntpWebRequest.NewsUriScheme.Equals(uri.Scheme))
-                                {
-                                    f.link = NntpWebRequest.NntpUriScheme + uri.CanonicalizedUri().Substring(uri.Scheme.Length);
-                                }
-                                else
-                                {
-                                    f.link = uri.CanonicalizedUri();
-                                }
-                            }
-                            catch (Exception)
-                            {
-                                isBadUri = true;
-                            }
+                        cat.Value = cat_trimmed;
+                        this.categories.Add(cat_trimmed, cat);
+                    }
+                }
+            }
 
-                            if (isBadUri)
+            //copy feeds over if we are importing a new feed  
+            if (myFeeds.feed != null)
+            {
+                foreach (NewsFeed f in myFeeds.feed)
+                {
+                    if (FeedsTable.ContainsKey(f.link) == false)
+                    {
+                        bool isBadUri = false;
+                        try
+                        {
+                            Uri uri = new Uri(f.link);
+                            // CLR 2.0 Uri does not like "news:" scheme, so we 
+                            // switch it to "nntp:" (see http://msdn2.microsoft.com/en-us/library/system.uri.scheme.aspx)
+                            if (NntpWebRequest.NewsUriScheme.Equals(uri.Scheme))
                             {
-                                continue;
+                                f.link = NntpWebRequest.NntpUriScheme + uri.CanonicalizedUri().Substring(uri.Scheme.Length);
                             }
                             else
                             {
-                                // test again: we may have changed to Uri above:
-                                if (FeedsTable.ContainsKey(f.link) == false)
+                                f.link = uri.CanonicalizedUri();
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            isBadUri = true;
+                        }
+
+                        if (isBadUri)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            // test again: we may have changed to Uri above:
+                            if (FeedsTable.ContainsKey(f.link) == false)
+                            {
+                                f.owner = this;
+                                this._feedsTable.Add(f.link, f);
+
+                                //add category if needed
+                                if (f.category != null)
                                 {
-                                    f.owner = this;
-                                    this._feedsTable.Add(f.link, f);
+                                    string cat_trimmed = f.category = f.category.Trim();
+
+                                    if (!this.Categories.ContainsKey(cat_trimmed))
+                                    {
+                                        this.AddCategory(cat_trimmed);
+                                    }
                                 }
                             }
                         }
                     }
                 }
+            }
 
-                //copy over category info if we are importing a new feed
-                if (myFeeds.categories != null)
+           
+            //copy over layout info if we are importing a new feed
+            if (myFeeds.listviewLayouts != null)
+            {
+                foreach (listviewLayout layout in myFeeds.listviewLayouts)
                 {
-                    foreach (category cat in myFeeds.categories)
+                    string layout_trimmed = layout.ID.Trim();
+                    if (!this.layouts.ContainsKey(layout_trimmed))
                     {
-                        string cat_trimmed = cat.Value.Trim();
-                        if (!this.Categories.ContainsKey(cat_trimmed))
-                        {
-                            cat.Value = cat_trimmed;
-                            this.categories.Add(cat_trimmed, cat);
-                        }
+                        this.layouts.Add(layout_trimmed, layout.FeedColumnLayout);
                     }
                 }
-
-                //This happens if for some reason the category of a feed didn't end up 
-                //in the categories collection during the last save of the feedlist. 
-                if (categoryMismatch && (myFeeds.feed != null))
-                {
-                    foreach (NewsFeed f in myFeeds.feed)
-                    {
-                        if (f.category != null)
-                        {
-                            string cat_trimmed = f.category = f.category.Trim();
-
-                            if (!this.Categories.ContainsKey(cat_trimmed))
-                            {
-                                this.AddCategory(cat_trimmed);
-                            }
-                        }
-                    }
-
-                    categoryMismatch = false;
-                }
-
-                //copy over layout info if we are importing a new feed
-                if (myFeeds.listviewLayouts != null)
-                {
-                    foreach (listviewLayout layout in myFeeds.listviewLayouts)
-                    {
-                        string layout_trimmed = layout.ID.Trim();
-                        if (!this.layouts.ContainsKey(layout_trimmed))
-                        {
-                            this.layouts.Add(layout_trimmed, layout.FeedColumnLayout);
-                        }
-                    }
-                }
+            }
 
                 //copy nntp-server defs. over if we are importing  
-                if (myFeeds.nntpservers != null)
+            if (myFeeds.nntpservers != null)
+            {
+                foreach (NntpServerDefinition sd in myFeeds.nntpservers)
                 {
-                    foreach (NntpServerDefinition sd in myFeeds.nntpservers)
+                    if (nntpServers.ContainsKey(sd.Name) == false)
                     {
-                        if (nntpServers.ContainsKey(sd.Name) == false)
-                        {
-                            nntpServers.Add(sd.Name, sd);
-                        }
+                        nntpServers.Add(sd.Name, sd);
                     }
                 }
+            }
 
                 //copy user-identities over if we are importing  
-                if (myFeeds.identities != null)
+            if (myFeeds.identities != null)
+            {
+                foreach (UserIdentity ui in myFeeds.identities)
                 {
-                    foreach (UserIdentity ui in myFeeds.identities)
+                    if (identities.ContainsKey(ui.Name) == false)
                     {
-                        if (identities.ContainsKey(ui.Name) == false)
-                        {
-                            identities.Add(ui.Name, ui);
-                        }
+                        identities.Add(ui.Name, ui);
                     }
                 }
+            }
 
                 //if refresh rate in imported feed then use that
-                if (myFeeds.refreshrateSpecified)
-                {
-                    this.refreshrate = myFeeds.refreshrate;
-                }
+            if (myFeeds.refreshrateSpecified)
+            {
+                this.refreshrate = myFeeds.refreshrate;
+            }
 
                 //if stylesheet specified in imported feed then use that
-                if (!string.IsNullOrEmpty(myFeeds.stylesheet))
-                {
-                    this.stylesheet = myFeeds.stylesheet;
-                }
+            if (!string.IsNullOrEmpty(myFeeds.stylesheet))
+            {
+                this.stylesheet = myFeeds.stylesheet;
+            }
 
                 //if download enclosures specified in imported feed then use that
-                if (myFeeds.downloadenclosuresSpecified)
-                {
-                    this.downloadenclosures = myFeeds.downloadenclosures;
-                }
+            if (myFeeds.downloadenclosuresSpecified)
+            {
+                this.downloadenclosures = myFeeds.downloadenclosures;
+            }
 
                 //if maximum enclosure cache size specified in imported feed then use that
-                if (myFeeds.enclosurecachesizeSpecified)
-                {
-                    this.enclosurecachesize = myFeeds.enclosurecachesize;
-                }
+            if (myFeeds.enclosurecachesizeSpecified)
+            {
+                this.enclosurecachesize = myFeeds.enclosurecachesize;
+            }
 
                 //if maximum number of enclosures to download on a new feed specified in imported feed then use that
-                if (myFeeds.numtodownloadonnewfeedSpecified)
-                {
-                    this.numtodownloadonnewfeed = myFeeds.numtodownloadonnewfeed;
-                }
+            if (myFeeds.numtodownloadonnewfeedSpecified)
+            {
+                this.numtodownloadonnewfeed = myFeeds.numtodownloadonnewfeed;
+            }
 
                 //if cause alert on enclosures specified in imported feed then use that
-                if (myFeeds.enclosurealertSpecified)
-                {
-                    this.enclosurealert = myFeeds.enclosurealert;
-                }
+            if (myFeeds.enclosurealertSpecified)
+            {
+                this.enclosurealert = myFeeds.enclosurealert;
+            }
 
                 //if create subfolders for enclosures specified in imported feed then use that
-                if (myFeeds.createsubfoldersforenclosuresSpecified)
-                {
-                    this.createsubfoldersforenclosures = myFeeds.createsubfoldersforenclosures;
-                }
+            if (myFeeds.createsubfoldersforenclosuresSpecified)
+            {
+                this.createsubfoldersforenclosures = myFeeds.createsubfoldersforenclosures;
+            }
 
 
                 //if marking items as read on exit specified in imported feed then use that
-                if (myFeeds.markitemsreadonexitSpecified)
-                {
-                    this.markitemsreadonexit = myFeeds.markitemsreadonexit;
-                }
+            if (myFeeds.markitemsreadonexitSpecified)
+            {
+                this.markitemsreadonexit = myFeeds.markitemsreadonexit;
+            }
 
                 //if enclosure folder specified in imported feed then use that
-                if (!string.IsNullOrEmpty(myFeeds.enclosurefolder))
-                {
-                    this.EnclosureFolder = myFeeds.enclosurefolder;
-                }
+            if (!string.IsNullOrEmpty(myFeeds.enclosurefolder))
+            {
+                this.EnclosureFolder = myFeeds.enclosurefolder;
+            }
 
                 //if podcast folder specified in imported feed then use that
-                if (!string.IsNullOrEmpty(myFeeds.podcastfolder))
-                {
-                    this.PodcastFolder = myFeeds.podcastfolder;
-                }
+            if (!string.IsNullOrEmpty(myFeeds.podcastfolder))
+            {
+                this.PodcastFolder = myFeeds.podcastfolder;
+            }
 
                 //if podcast file extensions specified in imported feed then use that
-                if (!string.IsNullOrEmpty(myFeeds.podcastfileexts))
-                {
-                    this.PodcastFileExtensionsAsString = myFeeds.podcastfileexts;
-                }
+            if (!string.IsNullOrEmpty(myFeeds.podcastfileexts))
+            {
+                this.PodcastFileExtensionsAsString = myFeeds.podcastfileexts;
+            }
 
 
                 //if listview layout specified in imported feed then use that
-                if (!string.IsNullOrEmpty(myFeeds.listviewlayout))
-                {
-                    this.listviewlayout = myFeeds.listviewlayout;
-                }
+            if (!string.IsNullOrEmpty(myFeeds.listviewlayout))
+            {
+                this.listviewlayout = myFeeds.listviewlayout;
+            }
 
                 //if max item age in imported feed then use that
-                try
+            try
+            {
+                if (!string.IsNullOrEmpty(myFeeds.maxitemage))
                 {
-                    if (!string.IsNullOrEmpty(myFeeds.maxitemage))
-                    {
-                        this.maxitemage = XmlConvert.ToTimeSpan(myFeeds.maxitemage);
-                    }
-                }
-                catch (FormatException fe)
-                {
-                    Trace("Error occured while parsing maximum item age from feed list: {0}", fe.ToString());
+                    this.maxitemage = XmlConvert.ToTimeSpan(myFeeds.maxitemage);
                 }
             }
+            catch (FormatException fe)
+            {
+                Trace("Error occured while parsing maximum item age from feed list: {0}", fe.ToString());
+            }
+            
         }
 
 
