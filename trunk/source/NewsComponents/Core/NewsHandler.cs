d14bc274cdcac39944149083057e79e14b5945d7
carnage4life <carnage4life@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-02-17 00:26:39 +0000

    Added support for using Windows RSS engine for downloading feeds in the background

--- trunk/source/NewsComponents/Core/NewsHandler.cs
+++ trunk/source/NewsComponents/Core/NewsHandler.cs
@@ -5777,7 +5777,7 @@ namespace NewsComponents
         /// or whether the cache can be used.</param>
         /// <remarks>This method uses the cache friendly If-None-Match and If-modified-Since
         /// HTTP headers when downloading feeds.</remarks>	
-        public void RefreshFeeds(bool force_download)
+        public virtual RefreshFeeds(bool force_download)
         {
             if (this.FeedsListOK == false)
             {
