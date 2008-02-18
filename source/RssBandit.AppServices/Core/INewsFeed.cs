ff6323715e8e96e838a786659234e4eb2fba6aa3
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2008-02-18 17:52:31 +0000

    Moved some enumerations to INewsFeed.

---
+++ source/RssBandit.AppServices/Core/INewsFeed.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+using System.Xml;
+
+namespace NewsComponents.Feed
+{
+    public interface INewsFeed : INotifyPropertyChanged
+    {
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        string title { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        string link { get; set; }
+
+        string id { get; set; }
+        bool lastretrievedSpecified { get; set; }
+        DateTime lastretrieved { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        int refreshrate { get; set; }
+
+        bool refreshrateSpecified { get; set; }
+        string etag { get; set; }
+        string cacheurl { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        string maxitemage { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        List<string> storiesrecentlyviewed { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        List<string> deletedstories { get; set; }
+
+        DateTime lastmodified { get; set; }
+        bool lastmodifiedSpecified { get; set; }
+        string authUser { get; set; }
+        Byte[] authPassword { get; set; }
+        string listviewlayout { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        string favicon { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        bool downloadenclosures { get; set; }
+
+        bool downloadenclosuresSpecified { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        string enclosurefolder { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        string stylesheet { get; set; }
+
+        int causedExceptionCount { get; set; }
+        bool causedException { get; set; }
+        bool replaceitemsonrefresh { get; set; }
+        bool replaceitemsonrefreshSpecified { get; set; }
+        string newsaccount { get; set; }
+        bool markitemsreadonexit { get; set; }
+        bool markitemsreadonexitSpecified { get; set; }
+        XmlElement[] Any { get; set; }
+        XmlAttribute[] AnyAttr { get; set; }
+        bool alertEnabled { get; set; }
+        bool alertEnabledSpecified { get; set; }
+        bool enclosurealert { get; set; }
+        bool enclosurealertSpecified { get; set; }
+        object Tag { get; set; }
+        object owner { get; set; } /* NewsHandler */ 
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        bool containsNewComments { get; set; }
+
+        /// <summary />
+        /// <remarks>Notifies on change. </remarks>
+        bool containsNewMessages { get; set; }
+
+        //TODO: Make this a collection
+        string category { get; set; }
+
+        /// <summary>
+        /// Removes an entry from the storiesrecentlyviewed collection
+        /// </summary>
+        /// <seealso cref="storiesrecentlyviewed"/>
+        /// <param name="storyid">The ID to add</param>
+        void AddViewedStory(string storyid);
+
+        /// <summary>
+        /// Adds an entry to the storiesrecentlyviewed collection
+        /// </summary>
+        /// <seealso cref="storiesrecentlyviewed"/>
+        /// <param name="storyid">The ID to remove</param>
+        void RemoveViewedStory(string storyid);
+
+        /// <summary>
+        /// Adds an entry from the deletedstories collection
+        /// </summary>
+        /// <seealso cref="deletedstories"/>
+        /// <param name="storyid">The ID to add</param>
+        void AddDeletedStory(string storyid);
+
+        /// <summary>
+        /// Remove an entry to the deletedstories collection
+        /// </summary>
+        /// <seealso cref="deletedstories"/>
+        /// <param name="storyid">The ID to remove</param>
+        void RemoveDeletedStory(string storyid);
+    }
+}
