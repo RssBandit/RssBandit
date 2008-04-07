fa36765e10295a4c09811ae44b4f4eb813c85c5a
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2008-04-07 00:19:34 +0000

    Took first steps towards NewsGator feed source

--- source/NewsComponents/Feed/NewsGatorFeedSource.cs
+++ source/NewsComponents/Feed/NewsGatorFeedSource.cs
@@ -10,7 +10,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
 using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 using NewsComponents.Net;
 using NewsComponents.Search;
@@ -19,18 +25,148 @@ namespace NewsComponents.Feed {
     /// <summary>
     /// A FeedSource that retrieves user subscriptions and feeds from NewsGator Online. 
     /// </summary>
-    class NewsGatorFeedSource : FeedSource{
+    class NewsGatorFeedSource : FeedSource
+    {
 
+        #region private fields
+
+
+        /// <summary>
+		/// The Newsgator Online sync token.
+		/// </summary>
+        private string NgosSyncToken; 
+     
+        /// <summary>
+        /// The NewsGator API key for RSS Bandit
+        /// </summary>
+        private static readonly string NgosProductKey = "7AF62582A5334A9CADF967818E734558";
+
+        /// <summary>
+        /// The name for this location. Used for identifying synchronization endpoints in the NewsGator Online UI
+        /// </summary>
+        private static readonly string NgosLocationName = "RssBandit-" + Environment.MachineName;
+        
+        /// <summary>
+        /// Service end point for the NewsGator Location REST API
+        /// </summary>
+        private static readonly string LocationApiUrl = "http://services.newsgator.com/ngws/svc/Location.aspx";
+
+        /// <summary>
+        /// Service end point for the NewsGator Subscription REST API
+        /// </summary>
+        private static readonly string SubscriptionApiUrl = "http://services.newsgator.com/ngws/svc/Subscription.aspx";
+
+        /// <summary>
+        /// Service end point for the NewsGator Feed REST API
+        /// </summary>
+       private static readonly string FeedApiUrl =  "http://services.newsgator.com/ngws/svc/Feed.aspx"; 
+
+        /// <summary>
+        /// Service end point for the NewsGator PostItem REST API
+        /// </summary>
+       private static readonly string PostItemApiUrl = "http://services.newsgator.com/ngws/svc/PostItem.aspx";
+
+       /// <summary>
+       /// Service end point for the NewsGator Folder REST API
+       /// </summary>
+       private static readonly string FolderApiUrl = "http://services.newsgator.com/ngws/svc/Folder.aspx"; 
+
+        #endregion 
+
+        #region constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeedSource"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="location">The user credentials</param>
+        internal NewsGatorFeedSource(INewsComponentsConfiguration configuration, SubscriptionLocation location)
+        {
+            this.p_configuration = configuration;
+            if (this.p_configuration == null)
+                this.p_configuration = FeedSource.DefaultConfiguration;
+
+            this.location = location;
+
+            //register with background thread for updating Newsgator Online
+            /* GoogleReaderUpdater.RegisterFeedSource(this); */ 
+
+           
+            // check for programmers error in configuration:
+            ValidateAndThrow(this.Configuration);
+
+            this.rssParser = new RssParser(this);
+
+            // initialize (later on loaded from feedlist):
+            this.PodcastFolder = this.Configuration.DownloadedFilesDataPath;
+            
+            if (String.IsNullOrEmpty(EnclosureFolder))
+            {
+                this.enclosureDownloader = new BackgroundDownloadManager(this.Configuration, this);
+                this.enclosureDownloader.DownloadCompleted += this.OnEnclosureDownloadComplete;
+            }
+
+            this.AsyncWebRequest = new AsyncWebRequest();
+            this.AsyncWebRequest.OnAllRequestsComplete += this.OnAllRequestsComplete;                   
+        }
+               
+
+        #endregion 
+
+
+        #region public properties
+
+        /// <summary>
+        /// Returns the NewsGator username associated with this feed source
+        /// </summary>
+        public string NewsGatorUserName
+        {
+            get { return this.location.Credentials.UserName; }
+        }
+
+        #endregion 
 
         #region public methods
 
-        #region feed list methods 
+        #region feed list methods
+
+        /// <summary>
+        /// Loads the user's feed list from NewsGator Online. In addition, sets the current NgosSyncToken property 
+        /// </summary>
+        /// <returns>The feed list from NewsGator Online</returns>
+        private feeds LoadFeedlistFromNewsGatorOnline()
+        {
+            /*
+            string feedlistUrl = apiUrlPrefix + "subscription/list";
+
+            //get the user's SID
+            this.AuthenticateUser();
+
+            //load feed list XML
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XmlReader.Create(AsyncWebRequest.GetSyncResponseStream(feedlistUrl, null, this.Proxy, MakeGoogleCookie(this.SID))));
+
+            var feedlist = from XmlNode node in doc.SelectNodes("/object/list[@name='subscriptions']/object")
+                           select MakeSubscription(node);
+
+            return feedlist;
+             */
+            return null; 
+        }
 
         /// <summary>
         /// Loads the feedlist from the FeedLocation. 
         ///</summary>
         public override void LoadFeedlist()
         {
+            //load Bandit subscriptions.xml document into memory
+            XmlReader reader = XmlReader.Create(this.location.Location);
+            XmlSerializer serializer = XmlHelper.SerializerCache.GetSerializer(typeof(feeds));
+            feeds myFeeds = (feeds)serializer.Deserialize(reader);
+            reader.Close();
+
+            //load feed list from Google Reader and use settings from subscriptions.xml
+            this.BootstrapAndLoadFeedlist(myFeeds);
         }
 
         /// <summary>
