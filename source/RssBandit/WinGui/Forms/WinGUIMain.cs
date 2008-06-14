f6825e66e36ee061bbc80a19eee6bdf4d377ec25
Oren Novotny <onovotny@users.noreply.github.com>
2008-06-14 20:23:41 +0000

    Fix version number of toolbars

--- source/RssBandit/WinGui/Forms/WinGUIMain.cs
+++ source/RssBandit/WinGui/Forms/WinGUIMain.cs
@@ -91,7 +91,7 @@ namespace RssBandit.WinGui.Forms
         /// If you forget this, you will always get your old toolbars layout
         /// restored from the users local machine.
         /// </remarks>
-        private const int _currentToolbarsVersion = 11;
+        private const int _currentToolbarsVersion = 14;
 
         /// <summary>
         /// To be raised by one on every UltraExplorerBar docks modification like new groups!
