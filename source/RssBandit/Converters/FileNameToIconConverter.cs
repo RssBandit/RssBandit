9e0427c4b97f8f0941808419b3c17a60ffd57ef3
Oren Novotny <onovotny@users.noreply.github.com>
2008-06-14 22:35:10 +0000

    Update item template

---
+++ source/RssBandit/Converters/FileNameToIconConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RssBandit.Converters
+{
+    class FileNameToIconConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(ImageSource))
+                throw new ArgumentException();
+
+            var fileName = value as string;
+            if(fileName != null)
+            {
+                return Win32.GetShellIconForFile(fileName);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
