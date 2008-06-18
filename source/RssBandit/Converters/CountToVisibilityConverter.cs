fbbe0fc905d684ffda82fef768ed67e719e0ceff
Oren Novotny <onovotny@users.noreply.github.com>
2008-06-18 18:43:40 +0000

    Added "no items present" text

---
+++ source/RssBandit/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RssBandit.Converters
+{
+    class CountToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility))
+                throw new ArgumentException();
+
+            if(value != null && value is int)
+            {
+                if ((int)value > 0)
+                    return Visibility.Visible;
+            }
+
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
