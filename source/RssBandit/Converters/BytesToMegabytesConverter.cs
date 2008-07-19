b691cc11846f700feb0469f192cc70dbd64581b5
Oren Novotny <onovotny@users.noreply.github.com>
2008-07-19 15:06:52 +0000

    A bit of download manager cleanup -- added pending state

---
+++ source/RssBandit/Converters/BytesToMegabytesConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace RssBandit.Converters
+{
+    internal sealed class BytesToMegabytesConverter : IValueConverter
+    {
+        private const double bytesPerMb = 1024 * 1024;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(value != null && value is long)
+            {
+                var size = (long)value;
+
+                double mb = size / bytesPerMb;
+
+                return string.Format(culture, "{0:n2} MB", mb);
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+}
