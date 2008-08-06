5c98fc72d5fd9b30e9cf50359e19d80f852aaa52
t_rendelmann <t_rendelmann@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-08-06 19:35:59 +0000

    added the missing build automation scripts

--- trunk/source/RssBandit.AppServices/VersionInfo.cs
+++ trunk/source/RssBandit.AppServices/VersionInfo.cs
@@ -14,9 +14,9 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.7.0.760")]
-[assembly: AssemblyFileVersion("1.7.0.760")]
-[assembly: AssemblyInformationalVersion("1.7.760")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
 [assembly: SatelliteContractVersion("1.7.0")]
 
 
