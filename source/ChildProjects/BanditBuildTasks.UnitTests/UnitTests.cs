ee9513387bb4d48fdb4d5f1b38639b3f24d11af7
t_rendelmann <netsrotr@users.noreply.github.com>
2008-08-06 18:29:24 +0000

    added new task for SVN repository path build; added unit test

---
+++ source/ChildProjects/BanditBuildTasks.UnitTests/UnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Org.RssBandit.MSBuild;
+
+namespace BanditBuildTasks.UnitTests
+{
+	/// <summary>
+	/// Summary description for TaskUnitTest
+	/// </summary>
+	[TestFixture]
+	public class TaskUnitTest
+	{
+		private string testDirectory;
+
+		[TestFixtureSetUp]
+		public void FixtureInit()
+		{
+			MockBuild buildEngine = new MockBuild();
+			testDirectory = TaskUtility.makeTestDirectory(buildEngine);
+		}
+
+		[Test(Description = "Test SVN Repository Path Task")]
+		public void TestSVNRepositoryTask()
+		{
+			string repoPath = @"d:\svn\repo\Test with spaces\trunk";
+
+			SvnRepositoryPath repositoryPath = new SvnRepositoryPath();
+			repositoryPath.BuildEngine = new MockBuild();
+
+			Assert.IsNotNull(repositoryPath);
+
+			repositoryPath.LocalPath = repoPath;
+			bool result = repositoryPath.Execute();
+
+			Assert.IsTrue(result);
+			Assert.IsFalse(String.IsNullOrEmpty(repositoryPath.SVNRepository ));
+
+			Console.WriteLine("Converted path '{0}' to '{1}'", repoPath, repositoryPath.SVNRepository);
+		}
+	}
+}
