c8df8f74bca72530d96dbfa0d9f712a6445310dd
Oren Novotny <onovotny@users.noreply.github.com>
2008-08-07 23:13:21 +0000

    fixup

--- build.daily.debug.cmd
+++ build.daily.debug.cmd
@@ -3,7 +3,7 @@
 set msbuild2=%windir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe
 set msbuild35=%windir%\Microsoft.NET\Framework\v3.5\msbuild.exe
 
-%msbuild35% MSBuildDefault.Proj /target:Dayly /m /property:Configuration=Debug /ToolsVersion:3.5
+%msbuild35% MSBuildDefault.Proj /target:Daily /m /property:Configuration=Debug;Platform=x86 /ToolsVersion:3.5
 
 pause
 
