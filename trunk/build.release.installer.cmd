2cdf00f7fcb530a4f883ab5355b30c9995929f1f
onovotny <onovotny@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-08-13 12:45:24 +0000

    Move platform var into proj from cmd

--- trunk/build.release.installer.cmd
+++ trunk/build.release.installer.cmd
@@ -3,7 +3,7 @@
 set msbuild2=%windir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe
 set msbuild35=%windir%\Microsoft.NET\Framework\v3.5\msbuild.exe
 
-%msbuild35% MSBuildDefault.Proj /m /target:Installer /property:Configuration=Release;Platform=x86 /ToolsVersion:3.5
+%msbuild35% MSBuildDefault.Proj /m /target:Installer /property:Configuration=Release
 
 pause
 
