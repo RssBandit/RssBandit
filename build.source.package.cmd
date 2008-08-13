b67439f028a0aebef83ffedcca49c26971f4de86
Oren Novotny <onovotny@users.noreply.github.com>
2008-08-13 12:45:24 +0000

    Move platform var into proj from cmd

--- build.source.package.cmd
+++ build.source.package.cmd
@@ -3,7 +3,7 @@
 set msbuild2=%windir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe
 set msbuild35=%windir%\Microsoft.NET\Framework\v3.5\msbuild.exe
 
-%msbuild35% MSBuildDefault.Proj /target:Source /m /property:Configuration=Debug /ToolsVersion:3.5
+%msbuild35% MSBuildDefault.Proj /target:Source /m /property:Configuration=Debug
 
 pause
 
