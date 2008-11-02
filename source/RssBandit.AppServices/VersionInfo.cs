fe2c5e30956087de9abc4ff1466fe4543dc7d8a4
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2008-11-02 05:49:45 +0000

    Final changes to support Phoenix release

--- source/RssBandit.AppServices/VersionInfo.cs
+++ source/RssBandit.AppServices/VersionInfo.cs
@@ -14,9 +14,9 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyVersion("1.8.0.855")]
+[assembly: AssemblyFileVersion("1.8.0.855")]
+[assembly: AssemblyInformationalVersion("1.8.855")]
 [assembly: SatelliteContractVersion("1.8.0")]
 
 
