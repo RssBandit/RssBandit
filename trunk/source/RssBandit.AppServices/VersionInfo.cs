65c2d045dfb2eda2cd176dd8c2c14683958b2330
carnage4life <carnage4life@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2008-11-11 03:12:08 +0000

    Another attempt at fixing feedlist migration. Replaced call to Path.GetTempFileName() since it actually creates a file. Will use Path.GetRandomFileName() instead.

--- trunk/source/RssBandit.AppServices/VersionInfo.cs
+++ trunk/source/RssBandit.AppServices/VersionInfo.cs
@@ -14,9 +14,9 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.8.0.868")]
-[assembly: AssemblyFileVersion("1.8.0.868")]
-[assembly: AssemblyInformationalVersion("1.8.868")]
+[assembly: AssemblyVersion("1.8.0.869")]
+[assembly: AssemblyFileVersion("1.8.0.869")]
+[assembly: AssemblyInformationalVersion("1.8.869")]
 [assembly: SatelliteContractVersion("1.8.0")]
 
 
