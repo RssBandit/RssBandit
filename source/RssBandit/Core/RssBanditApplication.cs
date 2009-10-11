f48a4bc753aa726eda5b1bb427d62c3ba0392972
Dare Obasanjo <Carnage4Life@users.noreply.github.com>
2009-10-11 19:18:28 +0000

    Fixed behavior of comment link when clicked on in Facebook reading pane. Also updated version info.

--- source/RssBandit/Core/RssBanditApplication.cs
+++ source/RssBandit/Core/RssBanditApplication.cs
@@ -100,7 +100,7 @@ namespace RssBandit
         /// </summary>
         /// <remarks>Next Final Release: remove the temp. preferences file 
         /// reading/writing before publishing!</remarks>
-        public static readonly string versionPostfix = "(SVN)";//String.Empty; // e.g. 'beta 1' or '(SVN)'
+        public static readonly string versionPostfix = String.Empty; // e.g. 'beta 1' or '(SVN)'
 
         private static bool validationErrorOccured;
         private static readonly RssBanditPreferences defaultPrefs = new RssBanditPreferences();
@@ -5193,6 +5193,10 @@ namespace RssBandit
 						INewsFeed f = GetFeed(entry, normalizedUrl);
                         if (f != null)
                             UnsubscribeFeed(f, false);
+                    }
+                     else if (webUrl.IndexOf("comment") != -1)
+                    {
+                        guiMain.PostReplyFromReadingPane(webUrl.Substring(idIndex));
                     }
                     return true;
                 }
