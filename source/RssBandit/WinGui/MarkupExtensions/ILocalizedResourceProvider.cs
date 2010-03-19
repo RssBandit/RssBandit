3b6a8e5a8a572c98e0e7cb071ef4b79c2628748d
t_rendelmann <t_rendelmann@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2010-03-19 06:36:18 +0000

    added missing files

---
+++ source/RssBandit/WinGui/MarkupExtensions/ILocalizedResourceProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace RssBandit.WinGui.MarkupExtensions
+{
+    public interface ILocalizedResourceProvider
+    {
+        /// <summary>
+        /// Gets the localized value for the specified key
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        object GetValue(string key);
+
+    }
+}
---
+++ source/RssBandit/WinGui/MarkupExtensions/ResourceFileProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.IO;
+using System.Windows;
+
+namespace RssBandit.WinGui.MarkupExtensions
+{
+    public class ResourceFileProvider : ResourceManager, ILocalizedResourceProvider
+    {
+        /// <summary>
+        /// Caches the current resource set
+        /// </summary>
+        private ResourceSet _resourceSet;
+
+        /// <summary>
+        /// Gets the localized value for the specified key
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public object GetValue(string key)
+        {
+            try
+            {
+                if (_resourceSet != null)
+                {
+                    return _resourceSet.GetObject(key);
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Loads the resources.
+        /// </summary>
+        private void LoadResources()
+        {
+            ReleaseAllResources();
+            _resourceSet = GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceFileProvider"/> class.
+        /// </summary>
+        /// <param name="baseName">Name of the base.</param>
+        /// <param name="assembly">The assembly.</param>
+        public ResourceFileProvider(string baseName, Assembly assembly)
+            : base( baseName, assembly )
+        {
+            LoadResources();
+            LocalizationManager.CultureChanged += (sender, e) => { LoadResources(); };
+        }
+
+       
+    }
+}
