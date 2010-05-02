7e244583e90afa84766ea248e475a3c1b75cbf26
t_rendelmann <netsrotr@users.noreply.github.com>
2010-05-02 15:01:32 +0000

    added simple view model for the tree view

---
+++ source/RssBandit/WinGui/ViewModel/TreeNodeViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿#region Version Info Header
+/*
+ * $Id$
+ * $HeadURL$
+ * Last modified by $Author$
+ * Last modified at $Date$
+ * $Revision$
+ */
+#endregion
+
+using System.Collections.ObjectModel;
+
+namespace RssBandit.WinGui.ViewModel
+{
+    public abstract class TreeNodeViewModelBase : ViewModelBase
+    {
+        private ObservableCollection<TreeNodeViewModelBase> _children = new ObservableCollection<TreeNodeViewModelBase>();
+
+        public abstract string Name
+        {
+            get; set;
+        }
+
+        public virtual ObservableCollection<TreeNodeViewModelBase> Children
+        {
+            get { return _children; }
+            set { _children = value; }
+        }
+    }
+}
