f7a611566236744e2bd2bd0b12bade5519c5e80f
carnage4life <carnage4life@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2010-07-07 13:35:47 +0000

    Added initial support for performing background UI tasks

--- trunk/source/RssBandit/WinGui/Forms/MainWindow.xaml.cs
+++ trunk/source/RssBandit/WinGui/Forms/MainWindow.xaml.cs
@@ -1,14 +1,25 @@
 ﻿
 
 using RssBandit.WinGui.Utility;
+using System;
 
 namespace RssBandit.WinGui.Forms
-{
+{ 
+
+     
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow 
     {
+        /// <summary>
+        /// Timer used for performing background UI tasks
+        /// </summary>
+        private UITaskTimer _uiTasksTimer = new UITaskTimer(); 
+
+        /// <summary>
+        /// Constructor initializes class
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -17,10 +28,144 @@ namespace RssBandit.WinGui.Forms
             Loaded += delegate
             {
                 Splash.Close();
+                _uiTasksTimer.Tick += new EventHandler(OnTasksTimerTick);
                 this.Loaded -= delegate { };
             };
-            
+
         }
-        
+
+
+        #region Timer related methods
+
+        /// <summary>
+        /// Callback for UITasksTimer
+        /// </summary>
+        private void OnTasksTimerTick(object sender, EventArgs e)
+        {
+
+            /* 
+            if (_uiTasksTimer[DelayedTasks.SyncRssSearchTree])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.SyncRssSearchTree);
+                PopulateTreeRssSearchScope();
+            }
+
+            if (_uiTasksTimer[DelayedTasks.RefreshTreeUnreadStatus])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.RefreshTreeUnreadStatus);
+                var param = (object[]) _uiTasksTimer.GetData(DelayedTasks.RefreshTreeUnreadStatus, true);
+                var tn = (TreeFeedsNodeBase) param[0];
+                var counter = (int) param[1];
+                if (tn != null)
+                    UpdateTreeNodeUnreadStatus(tn, counter);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.RefreshTreeCommentStatus])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.RefreshTreeCommentStatus);
+                var param = (object[]) _uiTasksTimer.GetData(DelayedTasks.RefreshTreeCommentStatus, true);
+                var tn = (TreeFeedsNodeBase) param[0];
+                var items = (IList<INewsItem>) param[1];
+                var commentsRead = (bool) param[2];
+                if (tn != null)
+                    UpdateCommentStatus(tn, items, commentsRead);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.NavigateToWebUrl])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.NavigateToWebUrl);
+                var param = (object[]) _uiTasksTimer.GetData(DelayedTasks.NavigateToWebUrl, true);
+                DetailTabNavigateToUrl((string) param[0], (string) param[1], (bool) param[2], (bool) param[3]);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.StartRefreshOneFeed])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.StartRefreshOneFeed);
+                var feedUrl = (string) _uiTasksTimer.GetData(DelayedTasks.StartRefreshOneFeed, true);
+                FeedSource source = FeedSourceOf(feedUrl); 
+                source.AsyncGetItemsForFeed(feedUrl, true, true);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.SaveUIConfiguration])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.SaveUIConfiguration);
+                SaveUIConfiguration(true);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.ShowFeedPropertiesDialog])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.ShowFeedPropertiesDialog);
+                var f = (INewsFeed) _uiTasksTimer.GetData(DelayedTasks.ShowFeedPropertiesDialog, true);
+                DisplayFeedProperties(f);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.NavigateToFeedNewsItem])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.NavigateToFeedNewsItem);
+                var item = (INewsItem) _uiTasksTimer.GetData(DelayedTasks.NavigateToFeedNewsItem, true);
+                NavigateToFeedNewsItem(item);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.NavigateToFeed])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.NavigateToFeed);
+                var f = (INewsFeed) _uiTasksTimer.GetData(DelayedTasks.NavigateToFeed, true);
+                NavigateToFeed(f);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.AutoSubscribeFeedUrl])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.AutoSubscribeFeedUrl);
+                var parameter = (object[]) _uiTasksTimer.GetData(DelayedTasks.AutoSubscribeFeedUrl, true);
+                AutoSubscribeFeed((TreeFeedsNodeBase) parameter[0], (string) parameter[1]);
+            }
+
+            if (_uiTasksTimer[DelayedTasks.ClearBrowserStatusInfo])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.ClearBrowserStatusInfo);
+                SetBrowserStatusBarText(String.Empty);
+                DeactivateWebProgressInfo();
+                _uiTasksTimer.Interval = 100; // reset interval 
+            }
+
+            if (_uiTasksTimer[DelayedTasks.InitOnFinishLoading])
+            {
+                _uiTasksTimer.StopTask(DelayedTasks.InitOnFinishLoading);
+                OnFinishLoading();
+            }
+
+            if (!_uiTasksTimer.AllTaskDone)
+            {
+                if (!_uiTasksTimer.Enabled)
+                    _uiTasksTimer.Start();
+            }
+            else
+            {
+                if (_uiTasksTimer.Enabled)
+                    _uiTasksTimer.Stop();
+            }
+             */
+        }
+
+        internal void DelayTask(DelayedTasks task)
+        {
+            DelayTask(task, null, new TimeSpan(0,0,0,0,100));
+        }
+
+        internal void DelayTask(DelayedTasks task, object data)
+        {
+            DelayTask(task, data, new TimeSpan(0, 0, 0, 0, 100));
+        }
+
+        internal void DelayTask(DelayedTasks task, object data, TimeSpan interval)
+        {
+            _uiTasksTimer.SetData(task, data);
+            if (_uiTasksTimer.Interval != interval)
+                _uiTasksTimer.Interval = interval;
+            _uiTasksTimer.StartTask(task);
+
+        }
+
+        #endregion 
     }
 }
