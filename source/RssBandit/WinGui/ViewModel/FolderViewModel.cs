2ae27d90ee7d118efeda95434b068c51f9f05fff
t_rendelmann <t_rendelmann@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2010-07-08 17:08:04 +0000

    added more treeview images and support for open/closed folder icons

--- source/RssBandit/WinGui/ViewModel/FolderViewModel.cs
+++ source/RssBandit/WinGui/ViewModel/FolderViewModel.cs
@@ -23,22 +23,27 @@ namespace RssBandit.WinGui.ViewModel
         {
             name.ExceptionIfNullOrEmpty("name");
             _name = name;
-            baseParent = parent;
-            _feedSource = source;
-            Type = FeedNodeType.Category; 
+            BaseParent = parent;
+            BaseFeedSource = source;
+            BaseImage = "/Resources/Images/TreeView/folder_closed.16.png";
+            Type = FeedNodeType.Category;
         }
 
         public override string Name
         {
             get { return _name; }
-            set { _name = value; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
         }
 
         public override string Category
         {
             get { 
                 string catName = _name;
-                TreeNodeViewModelBase parent = baseParent;
+                TreeNodeViewModelBase parent = BaseParent;
 
                 while (parent != null)
                 {
@@ -50,7 +55,19 @@ namespace RssBandit.WinGui.ViewModel
             }
             set { /* not required: we have the Name and the Parents to calculate it */ }
         }
-       
-      
+
+        public override bool IsExpanded
+        {
+            get
+            {
+                return base.IsExpanded;
+            }
+            set
+            {
+                base.IsExpanded = value;
+                Image = value ? "/Resources/Images/TreeView/folder.16.png" :
+                    "/Resources/Images/TreeView/folder_closed.16.png";
+            }
+        }
     }
 }
