e06a1733fefd48dc1b95a8685a3d1c03959edede
onovotny <onovotny@ebbda88f-94c6-4aba-9098-8a59085c8bc7>
2014-01-15 23:35:05 +0000

    Add missing files to build bandit

---
+++ branches/v1.9.x/source/NewsComponents/Net/HttpCookieManager.cs
@@ -0,0 +1,123 @@
+#region Version Info Header
+/*
+ * $Id$
+ * $HeadURL$
+ * Last modified by $Author$
+ * Last modified at $Date$
+ * $Revision$
+ */
+#endregion
+
+using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using NewsComponents.Utils;
+using RssBandit.Common;
+using RssBandit.Common.Logging;
+using log4net;
+
+namespace NewsComponents.Net
+{
+    /// <summary>
+    /// Used to manage cookies
+    /// </summary>
+    /// <remarks>see http://www.rendelmann.info/blog/CommentView.aspx?guid=bd99bcd5-7088-4d46-801e-c0fe622dc2e5</remarks>
+    internal class HttpCookieManager
+    {
+       // private static readonly ILog _log = DefaultLog.GetLogger(typeof(HttpCookieManager));
+
+        /// <summary>
+        /// Retrieves the cookie(s) from windows system and assign them to the request, 
+        /// if available.
+        /// </summary>
+        /// <param name="request">HttpWebRequest</param>
+        public static void SetCookies(HttpWebRequest request)
+        {
+            CookieContainer c = GetCookieContainerUri(request.RequestUri);
+            if (c.Count > 0)
+                request.CookieContainer = c;
+        }
+
+        /// <summary>
+        /// Gets newly received cookie(s) and make them persistent within windows system.
+        /// </summary>
+        /// <param name="response">HttpWebResponse</param>
+        public static void GetCookies(HttpWebResponse response)
+        {
+            if (response.Headers["Set-Cookie"] != null)
+            {
+                /* 
+				 * It seems this may log users out of certain sites, 
+				 * see http://www.rssbandit.org/forum/topic.asp?whichpage=1&TOPIC_ID=2080&#4080
+				 *	- InternetSetCookie(response.ResponseUri.CanonicalizedUri(), null, response.Headers["Set-Cookie"]);
+				 */
+            }
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool InternetGetCookie(
+            string lpszUrl, string lpszCookieName, StringBuilder lpCookieData, ref int lpdwSize);
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool InternetSetCookie(
+            string lpszUrl, string lpszCookieName, string lpszCookieData);
+
+        private static CookieContainer GetCookieContainerUri(Uri url)
+        {
+            CookieContainer container = new CookieContainer();
+            string cookieHeaders = RetrieveIECookiesForUrl(url.CanonicalizedUri());
+            if (cookieHeaders.Length > 0)
+            {
+                try
+                {
+                    container.SetCookies(url, cookieHeaders);
+                }
+                catch (CookieException ce)
+                {
+                    //we might get an error on malformed cookies
+                    //_log.Error(
+                    //    String.Format("GetCookieContainerUri() exception parsing '{0}' for url '{1}'", cookieHeaders,
+                    //                  url.CanonicalizedUri()), ce);
+                }
+            }
+            return container;
+        }
+
+
+        private static string RetrieveIECookiesForUrl(string url)
+        {
+            StringBuilder cookieHeader = new StringBuilder(new String(' ', 256), 256);
+            int datasize = cookieHeader.Length;
+            if (!InternetGetCookie(url, null, cookieHeader, ref datasize))
+            {
+                if (datasize < 0)
+                    return String.Empty;
+                cookieHeader = new StringBuilder(datasize); // resize with new datasize
+                InternetGetCookie(url, null, cookieHeader, ref datasize);
+            }
+            return FixupIECookies(cookieHeader);
+        }
+
+        /// <summary>
+        /// Fixups the cookies IE may return. 
+        /// If there is a semicolon, we replace it with a comma because 
+        /// .NET requires comma as a cookie separators.        
+        /// If there is a comma AND a semicolon, we escape the comma
+        /// first, then replace the semicolon with a comma 
+        /// </summary>
+        /// <param name="b">The b.</param>
+        /// <returns></returns>
+        private static string FixupIECookies(StringBuilder b)
+        {
+            string s = b.ToString();
+            if (s.IndexOf(",") >= 0 || s.IndexOf(";") >= 0)
+            {
+                s = s.Replace(",", escapedComma).Replace(";", ",");
+            }
+            return s;
+        }
+
+        private static readonly string escapedComma = HtmlHelper.UrlEncode(",");
+    }
+}
---
+++ branches/v1.9.x/source/NewsComponents/Threading/PrioritizingTaskScheduler.cs
@@ -0,0 +1,251 @@
+﻿//--------------------------------------------------------------------------
+// 
+//  Some Parts Copyright (c) Microsoft Corporation.  All rights reserved. 
+// 
+//  File: PrioritizingTaskScheduler.cs
+//
+//--------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using log4net;
+using NewsComponents.Utils;
+using RssBandit.Common.Logging;
+
+namespace System.Threading.Tasks.Schedulers
+{
+    /// <summary>
+    ///   Provides a task scheduler that supports reprioritizing previously queued tasks.
+    /// </summary>
+    public sealed class PrioritizingTaskScheduler : TaskScheduler
+    {
+       // private static readonly ILog Log = DefaultLog.GetLogger(typeof (PrioritizingTaskScheduler));
+
+        /// <summary>
+        ///   Whether the current thread is processing work items.
+        /// </summary>
+        [ThreadStatic] private static bool _currentThreadIsProcessingItems;
+
+        /// <summary>
+        ///   Whether the scheduler is currently processing work items.
+        /// </summary>
+        private int _delegatesQueuedOrRunning; // protected by lock(_tasks)       
+
+        /// <summary>
+        ///   The maximum concurrency level allowed by this scheduler.
+        /// </summary>
+        private readonly int _maxDegreeOfParallelism = 4;
+
+        private readonly LinkedList<Task> _tasks = new LinkedList<Task>(); // protected by lock(_tasks)
+
+        /// <summary>
+        ///   Initializes a new instance of this class whose max degree of parallelism is read from the RSSBandit.exe.config file. Default value is 4 if no 
+        ///   configured value found.
+        /// </summary>
+        public PrioritizingTaskScheduler()
+        {
+            try
+            {
+                int newMax = Common.Configuration.ReadAppSettingsEntry("MaxDownloadThreads", _maxDegreeOfParallelism);
+                if (newMax > 0 && newMax < 50)
+                {
+                    _maxDegreeOfParallelism = newMax;
+                }
+            }
+            catch (ConfigurationErrorsException cex)
+            {
+                Log.Error("Failed to read 'MaxDownloadThreads' from .config", cex);
+            }
+        }
+
+        /// <summary>
+        ///   Initializes an instance of the LimitedConcurrencyLevelTaskScheduler class with the
+        ///   specified degree of parallelism.
+        /// </summary>
+        /// <param name = "maxDegreeOfParallelism">The maximum degree of parallelism provided by this scheduler.</param>
+        public PrioritizingTaskScheduler(int maxDegreeOfParallelism)
+        {
+            if (maxDegreeOfParallelism < 1) throw new ArgumentOutOfRangeException("maxDegreeOfParallelism");
+            _maxDegreeOfParallelism = maxDegreeOfParallelism;
+        }
+
+        /// <summary>
+        ///   Gets the maximum concurrency level supported by this scheduler.
+        /// </summary>
+        public override sealed int MaximumConcurrencyLevel
+        {
+            get { return _maxDegreeOfParallelism; }
+        }
+
+        /// <summary>
+        ///   Reprioritizes a previously queued task to the back of the queue.
+        /// </summary>
+        /// <param name = "task">The task to be reprioritized.</param>
+        /// <returns>Whether the task could be found and moved to the back of the queue.</returns>
+        public bool Deprioritize(Task task)
+        {
+            lock (_tasks)
+            {
+                LinkedListNode<Task> node = _tasks.Find(task);
+                if (node != null)
+                {
+                    _tasks.Remove(node);
+                    _tasks.AddLast(node);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///   Reprioritizes a previously queued task to the front of the queue.
+        /// </summary>
+        /// <param name = "task">The task to be reprioritized.</param>
+        /// <returns>Whether the task could be found and moved to the front of the queue.</returns>
+        public bool Prioritize(Task task)
+        {
+            lock (_tasks)
+            {
+                LinkedListNode<Task> node = _tasks.Find(task);
+                if (node != null)
+                {
+                    _tasks.Remove(node);
+                    _tasks.AddFirst(node);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///   Gets all of the tasks currently queued to the scheduler.
+        /// </summary>
+        /// <returns>An enumerable of the tasks currently queued to the scheduler.</returns>
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            bool lockTaken = false;
+            try
+            {
+                lockTaken = Monitor.TryEnter(_tasks);
+                if (lockTaken) return _tasks.ToArray();
+                else throw new NotSupportedException();
+            }
+            finally
+            {
+                if (lockTaken) Monitor.Exit(_tasks);
+            }
+        }
+
+        /// <summary>
+        ///   Queues a task to the scheduler.
+        /// </summary>
+        /// <param name = "task">The task to be queued.</param>
+        protected override void QueueTask(Task task)
+        {
+            // Add the task to the list of tasks to be processed.  If there aren't enough
+            // delegates currently queued or running to process tasks, schedule another.
+            lock (_tasks)
+            {
+                _tasks.AddLast(task);
+                if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
+                {
+                    ++_delegatesQueuedOrRunning;
+                    NotifyThreadPoolOfPendingWork();
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Removes a previously queued item from the scheduler.
+        /// </summary>
+        /// <param name = "task">The task to be removed.</param>
+        /// <returns>Whether the task could be removed from the scheduler.</returns>
+        protected override bool TryDequeue(Task task)
+        {
+            lock (_tasks) return _tasks.Remove(task);
+        }
+
+        /// <summary>
+        ///   Attempts to execute the specified task on the current thread.
+        /// </summary>
+        /// <param name = "task">The task to be executed.</param>
+        /// <param name = "taskWasPreviouslyQueued"></param>
+        /// <returns>Whether the task could be executed on the current thread.</returns>
+        protected override sealed bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            // If this thread isn't already processing a task, we don't support inlining
+            if (!_currentThreadIsProcessingItems) return false;
+
+            // If the task was previously queued, remove it from the queue
+            if (taskWasPreviouslyQueued) TryDequeue(task);
+
+            // Try to run the task.
+            return base.TryExecuteTask(task);
+        }
+
+        /// <summary>
+        ///   Informs the ThreadPool that there's work to be executed for this scheduler.
+        /// </summary>
+        private void NotifyThreadPoolOfPendingWork()
+        {
+            ThreadPool.UnsafeQueueUserWorkItem(_ =>
+                                                   {
+                                                       // Note that the current thread is now processing work items.
+                                                       // This is necessary to enable inlining of tasks into this thread.
+                                                       _currentThreadIsProcessingItems = true;
+                                                       try
+                                                       {
+                                                           // Process all available items in the queue.
+                                                           while (true)
+                                                           {
+                                                               Task item;
+                                                               lock (_tasks)
+                                                               {
+                                                                   // When there are no more items to be processed,
+                                                                   // note that we're done processing, and get out.
+                                                                   if (_tasks.Count == 0)
+                                                                   {
+                                                                       --_delegatesQueuedOrRunning;
+                                                                       break;
+                                                                   }
+
+                                                                   // Get the next item from the queue
+                                                                   item = _tasks.First.Value;
+                                                                   _tasks.RemoveFirst();
+                                                               }
+
+                                                               // Execute the task we pulled out of the queue
+                                                               base.TryExecuteTask(item);
+                                                           }
+                                                       }
+                                                           // We're done processing items on the current thread
+                                                       finally
+                                                       {
+                                                           _currentThreadIsProcessingItems = false;
+                                                       }
+                                                   }, null);
+        }
+
+        /// <summary>
+        ///   Picks up and executes the next item in the queue.
+        /// </summary>
+        /// <param name = "ignored">Ignored.</param>
+        private void ProcessNextQueuedItem(object ignored)
+        {
+            Task t;
+            lock (_tasks)
+            {
+                if (_tasks.Count > 0)
+                {
+                    t = _tasks.First.Value;
+                    _tasks.RemoveFirst();
+                }
+                else return;
+            }
+            base.TryExecuteTask(t);
+        }
+    }
+}
